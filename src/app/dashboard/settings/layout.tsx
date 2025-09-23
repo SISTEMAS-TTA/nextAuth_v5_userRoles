762ef064c2ea9a3543e8f95434dfb8a309d80437
SISTEMAS-TTA <sistemas@ttarquitectos.mx>
2025-09-23 17:55:21 -0600

    update: update project

---
+++ src/app/dashboard/settings/layout.tsx
@@ -0,0 +1,7 @@
+const DashboardLayout = ({ children }: {children:React.ReactNode}) => {
+    <div className="flex flex-col gap-y-4">
+        <nav></nav>
+    </div>
+}
+
+export default DashboardLayout;
